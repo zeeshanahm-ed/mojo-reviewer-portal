d26fc5f0f332d530fc3288645fa6fa10c41e9036
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-09-16 10:01:11 +0500

    Update deployment script for reviewer portal and remove unused media files

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ fi
 cp envs/.env.prod .env
 npm install
 npm run build
-aws s3 sync  --profile mojo-app ./dist s3://mojo-admin-dashboard
-aws cloudfront create-invalidation --profile mojo-app --distribution-id E36EJJPEZKZYME --paths "/*"
+aws s3 sync  --profile mojo-app ./dist s3://mojo-reviewer-portal
+aws cloudfront create-invalidation --profile mojo-app --distribution-id E3Q4KL24TR9WOR --paths "/*"
 # cp envs/.env.local .env
